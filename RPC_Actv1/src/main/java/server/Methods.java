e244d719d9a0afce6d9a321a7da458d024872c19
AlexxJoel <20213tn019@utez.edu.mx>
2022-09-09 09:39:41 -0500

    Suma de dos numeros RCP

--- RPC_Actv1/src/main/java/server/Methods.java
+++ RPC_Actv1/src/main/java/server/Methods.java
@@ -1,4 +1,13 @@
 package server;
 
 public class Methods {
+
+
+
+
+    //Method to retun of additions
+    public double addition(double num1, double num2){
+        return num1 + num2;
+    }
+
 }
--- RPC_Actv1/src/main/java/server/RPCServer.java
+++ RPC_Actv1/src/main/java/server/RPCServer.java
@@ -1,4 +1,35 @@
 package server;
 
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.server.PropertyHandlerMapping;
+import org.apache.xmlrpc.webserver.WebServer;
+
+import java.io.IOException;
+
 public class RPCServer {
+
+    public static void main(String[] args) throws XmlRpcException, IOException {
+
+        System.out.println("Initialize server....");
+        //KEY WORD TO APUNATR A LOS MEOTDOS
+        //Request en servlets
+        PropertyHandlerMapping mapping = new PropertyHandlerMapping();
+
+        mapping.addHandler("Methods", Methods.class); // Asiganos una variable a la clase
+
+        //El class hace referencia a la clase compilada
+
+        WebServer server = new WebServer(1200); //Abrimos el puerto donde abrira ek server
+
+        server.getXmlRpcServer().setHandlerMapping(mapping); // seteamos la clase
+
+        server.start(); //empezar el servidor
+
+        System.out.println("Server running in http://localhost:1200");
+
+        System.out.println("Waiting requests...");
+
+
+    }
+
 }
