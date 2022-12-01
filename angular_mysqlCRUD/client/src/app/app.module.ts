20c3dcdceacf1a4e4325b34c69b93b142763c96f
AlexxJoel <20213tn019@utez.edu.mx>
2022-12-01 13:24:21 -0600

    Crud terminado boostrap

--- angular_mysqlCRUD/client/src/app/app.module.ts
+++ angular_mysqlCRUD/client/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +9,7 @@ import { NavigationComponent } from './components/navigation/navigation.componen
 import { GameFormComponent } from './components/game-form/game-form.component';
 import { GameListComponent } from './components/game-list/game-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GamesService} from "./services/games.service";
 
 @NgModule({
   declarations: [
@@ -18,9 +21,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    GamesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular_mysqlCRUD/client/src/app/components/game-form/game-form.component.ts
+++ angular_mysqlCRUD/client/src/app/components/game-form/game-form.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
+import {Game} from "../../models/game";
+import {GamesService} from "../../services/games.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-game-form',
   templateUrl: './game-form.component.html',
   styleUrls: ['./game-form.component.css']
 })
-export class GameFormComponent {
+export class GameFormComponent implements OnInit{
 
+  @HostBinding('class') classes = 'row'
+
+  game: Game = {
+    id:0,
+    title:"",
+    description:"",
+    image:'',
+    created_at: new Date()
+  };
+  edit:boolean = false ;
+
+  constructor(private gameService:GamesService, private  rout:Router, private activeRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    const params = this.activeRoute.snapshot.params;
+    if (params['id']){
+      console.log(params['id'])
+      this.gameService.getGame(params['id']).subscribe(
+        res=> {
+          this.game = res,
+            this.edit = true;
+        },
+        err=>console.log(err)
+      )
+    }
+  }
+
+  saveNewGame(){
+    delete this.game.created_at;
+    delete this.game.id;
+    this.gameService.saveGame(this.game).subscribe(
+      res=>{
+        this.rout.navigate(['/games'])
+      },
+      err=> console.log(err)
+    );
+  }
+
+  updateGame(){
+    delete this.game.created_at;
+    this.gameService.updateGame(this.game.id , this.game).subscribe(
+      res=>{
+        console.log("the game was updated")
+        this.rout.navigate(['/games'])
+      },
+      error => console.log(error)
+    )
+  }
 }
